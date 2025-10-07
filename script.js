f97034245abf046d49c9487964571994354d6662
177159 <henick408@gmail.com>
2025-10-07 09:52:21 +0000

    COM1_1.2

--- script.js
+++ script.js
@@ -5,4 +5,22 @@
       document.getElementById("ex1_content").textContent = numbers;
     }
   );
+
+  const content = document.getElementById("ex2_content");
+  const input = document.getElementById("ex2_text")
+  input.addEventListener("input",
+    function() {
+      const value = input.value;
+      if (value.length != 9) {
+        content.textContent = "Dlugosc musi byc rowna 9";
+      } else if (/[a-zA-Z]/.test(value)) {
+        content.textContent = "Nie moze zawierac liter";
+      } else if (/[^0-9]/.test(value)) {
+        content.textContent = "Nie moze zawierac znakow specjalnych";
+      } else {
+        content.textContent = "git";
+      }
+    }
+  );
+
 })();
